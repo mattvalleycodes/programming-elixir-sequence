50a3540c7619a098db3925da3557a9699711ced2
mehdivk <valikhani@sirangweb.com>
2017-07-13 21:37:16 +1000

    upgraded state structure

--- lib/sequence/server.ex
+++ lib/sequence/server.ex
@@ -1,7 +1,12 @@
 defmodule Sequence.Server do
   use GenServer
+  require Logger
 
-  @vsn "0"
+  @vsn "1"
+
+  defmodule State do
+    defstruct current_number: 0, stash_pid: nil, delta: 1
+  end
 
   def start_link(stash_pid) do
     { :ok, _pid } = GenServer.start_link(__MODULE__, stash_pid, name: __MODULE__)
@@ -16,20 +21,40 @@ defmodule Sequence.Server do
     GenServer.cast __MODULE__, { :increment_number, delta }
   end
 
-  def handle_call(:next_number, _from, {current_number, stash_pid}) do
-    { :reply, current_number, {current_number + 1, stash_pid} }
+  def handle_call(:next_number, _from, state) do
+    {
+      :reply,
+      state.current_number,
+      %{ state | current_number: state.current_number + state.delta}
+    }
   end
 
-  def handle_cast({ :increment_number, delta }, {current_number, stash_pid}) do
-    { :noreply, {current_number + delta, stash_pid} }
+  def handle_cast({ :increment_number, delta }, state) do
+    {
+      :noreply,
+      %{ state | current_number: state.current_number + delta, delta: delta }
+    }
   end
 
   def init(stash_pid) do
     current_number = Sequence.Stash.get_value stash_pid
-    { :ok, {current_number, stash_pid} }
+    {
+      :ok,
+      %State{current_number: current_number , stash_pid: stash_pid}
+    }
   end
 
-  def terminate(_reason, {current_number, stash_pid}) do
-    Sequence.Stash.save_value stash_pid, current_number
+  def terminate(_reason, state) do
+    Sequence.Stash.save_value state.stash_pid, state.current_number
+  end
+
+  def code_change("0", old_state = { current_number, stash_pid },  _extra) do
+    new_state = %State{ current_number: current_number, stash_pid: stash_pid, delta: 1 }
+
+    Logger.info "Changing code from 0 to 1"
+    Logger.info inspect(old_state)
+    Logger.info inspect(new_state)
+
+    {:ok, new_state}
   end
 end
